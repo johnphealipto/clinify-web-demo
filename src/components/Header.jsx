31f216f92f92911af273337383682256eb4b694b
John Adibe <johnadibe450@gmail.com>
2022-06-20 17:27:08 +0100

    Worked on responsiveness :zap:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import styles from '../assets/styles/Header.module.css'
 import Button from './Button'
+import { IoMdMenu } from 'react-icons/io';
 
 const Header = () => {
 
@@ -10,7 +11,7 @@ const Header = () => {
     <header className={styles.header}>
       <div className={styles.container}>
         <h3>E Wallet</h3>
-        <nav>
+        <nav className={styles.lg_nav}>
           <ul className={styles.menu_list}>
             {menu.map((item, i) => 
               <li 
@@ -22,7 +23,14 @@ const Header = () => {
             )}
           </ul>
         </nav>
-        <Button style='box-lemon' label="Sign in" />
+        <div className={styles.login_btn}>
+          <Button style='box-lemon' label="Sign in" />
+        </div>
+        <div className={styles.menu_btn}>
+          <button type='button'>
+            <IoMdMenu size={30} />
+          </button>
+        </div>
       </div>
     </header>
   )
