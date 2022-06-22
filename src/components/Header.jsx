818ec7b7b1a88d90303722960b65a3f40c1955e1
John Adibe <johnadibe450@gmail.com>
2022-06-22 19:20:27 +0100

    Worked on the nav responsiveness :zap:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,71 @@
+import { useState } from 'react';
 import Link from 'next/link'
+import { useRouter } from 'next/router';
 import styles from '../assets/styles/Header.module.css'
 import Button from './Button'
 import { IoMdMenu } from 'react-icons/io';
 
 const Header = () => {
+  const router = useRouter()
 
-  const menu = ["Product", "Pricing", "About"]
+  const [toggleNav, setToggleNav] = useState(false)
+
+  const menu = [
+    {
+      name: "Home",
+      path: "/"
+    },
+    {
+      name: "Product",
+      path: "/products"
+    },
+    {
+      name: "Solutions",
+      path: "/products"
+    },
+    {
+      name: "Contact",
+      path: "/products"
+    },
+    {
+      name: "Blog",
+      path: "https://blog.myclinify.com/"
+    },
+  ]
 
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <h3>E Wallet</h3>
-        <nav className={styles.lg_nav}>
+        <nav className={[styles.nav, toggleNav ? styles.slide : ""].join(" ")}>
           <ul className={styles.menu_list}>
             {menu.map((item, i) => 
               <li 
                 key={i}
-                className={styles.menu_item}
+                className={[styles.menu_item, router.pathname === item.path ? styles.active_link : ""].join(" ")}
+                onClick={() => setToggleNav(!toggleNav)}
               >
-                <Link href="/">{item}</Link>
+                <Link href={item.path}>
+                  {item.name}
+                </Link>
               </li>
             )}
           </ul>
+          <div
+            className={styles.sm_login_btn}
+            onClick={() => setToggleNav(!toggleNav)}
+          >
+            <Button style='box-lemon' label="Sign in" />
+          </div>
         </nav>
-        <div className={styles.login_btn}>
+        <div className={styles.lg_login_btn}>
           <Button style='box-lemon' label="Sign in" />
         </div>
         <div className={styles.menu_btn}>
-          <button type='button'>
+          <button 
+            type='button'
+            onClick={() => setToggleNav(!toggleNav)}
+          >
             <IoMdMenu size={30} />
           </button>
         </div>
